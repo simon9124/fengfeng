2211b1c125a127de0d569be719a300bee911ffb0
simon9124 <435776313@qq.com>
2020-04-24 17:56:38 +0800

    打包部署

--- src/router/_import_development.js
+++
@@ -1,2 +0,0 @@
-// module.default = file => require("@/view/" + file + ".vue").default; // vue-loader at least v13.0.0+
-module.exports = file => require("@/view/" + file + ".vue").default; // vue-loader at least v13.0.0+
